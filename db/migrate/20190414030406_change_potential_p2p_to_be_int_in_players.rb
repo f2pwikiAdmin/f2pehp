19deabb5770f03dcfc9644d3d0af72afa9f9fca2
Stward Pérez Cabezas <stwardperezc@protonmail.com>
2025-11-24 23:26:38 -0600

    Fix `potential_p2p` migration error

--- db/migrate/20190414030406_change_potential_p2p_to_be_int_in_players.rb
+++ db/migrate/20190414030406_change_potential_p2p_to_be_int_in_players.rb
@@ -1,5 +1,5 @@
 class ChangePotentialP2pToBeIntInPlayers < ActiveRecord::Migration[4.2]
   def change
-    change_column :players, :potential_p2p, :float
+    change_column :players, :potential_p2p, :float, using: 'potential_p2p::double precision'
   end
 end
